f4f1136d8cd3a317d273c675bc73c52ecd97f6d5
Bilal Shahbaz <shahbazbilal46@gmail.com>
2021-09-23 20:09:18 +0500

    init

---
+++ Delegate(Event Handler)/PracticeConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PracticeConsole
+{
+    public delegate string MyDel(string str);
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Enter the video Title:");
+            var videoTitle = Console.ReadLine();
+
+            Video newVideo = new Video { Title = videoTitle };
+
+            var VEncoder = new VideoEncoder(); //provider
+            var mailService = new MailingService(); //subscriber
+            var messageService = new MessageService(); //subscriber
+
+            VEncoder.VideoEncodedEvent += mailService.OnVideoEncoded;  
+            VEncoder.VideoEncodedEvent += messageService.OnVideoEncoded;
+
+            VEncoder.Encode(newVideo);
+
+        }
+
+    }
+}
